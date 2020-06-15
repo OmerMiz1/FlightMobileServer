415168b5c7585e89205ed5408dfa4df7f04c389a
OmerMizrachi <omermizra@gmail.com>
2020-06-15 23:00:25 +0300

    Added some model files from EX1

---
+++ FlightMobileServer/ServerModels/AsyncCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using FlightMobileServer.Models;
+
+namespace FlightMobileServer.ServerModels {
+    public enum Result { Ok, NotOk }
+    public class AsyncCommand {
+        public AsyncCommand() { }
+        public AsyncCommand(Command command, TaskCompletionSource<Result> completion) {
+            Command = command;
+            Completion = completion;
+        }
+
+        public Command Command { get; private set; }
+        public Task<Result> Task { get => Completion.Task; }
+        public TaskCompletionSource<Result> Completion { get; private set; }
+
+        public AsyncCommand (Command input) { 
+            Command = input;
+
+            // Watch out! Run Continuations Async is important!
+            Completion = new TaskCompletionSource<Result> (
+            TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+    }
+}
---
+++ FlightMobileServer/ServerModels/ITcpClient.cs
@@ -0,0 +1,10 @@
+﻿namespace FlightMobileServer.ServerModels
+{
+    public interface ITcpClient
+    {
+        void Connect(string ip, int port);
+        void Write(string command);
+        string Read();
+        void Disconnect();
+    }
+}
