c1c6602586cfd6d1770436bead5dace3dc63d5e6
OmerMizrachi <omermizra@gmail.com>
2020-06-15 15:52:38 +0300

    Added 'Command' model

--- FlightMobileServer/Models/Command.cs
+++ FlightMobileServer/Models/Command.cs
@@ -4,23 +4,27 @@ using Newtonsoft.Json;
 
 namespace FlightMobileServer.Models {
     public class Command {
+
         [JsonProperty("aileron")]
-        // [Range(-1,1)]
+        [Range(-1, 1, ErrorMessage = _aileronOutOfRangeMsg)]
         public double Aileron { get; set; }
+
         [JsonProperty("rudder")]
-        // [Range(-1, 1)]
+        [Range(-1, 1, ErrorMessage = _rudderOutOfRangeMsg)]
         public double Rudder { get; set; }
+
         [JsonProperty("elevator")]
-        // [Range(-1,1)]
+        [Range(-1, 1, ErrorMessage = _elevatorOutOfRangeMsg)]
         public double Elevator{ get; set; }
+
         [JsonProperty("throttle")]
-        // [Range(0,1)]
+        [Range(0, 1, ErrorMessage = _throttleOutOfRangeMsg)]
         public double Throttle { get; set; }
 
-
-        /*private string aileronOutOfRangeMsg = "aileron must be between -1 to 1."; //debug
-        private string rudderOutOfRangeMsg = "rudder must be between -1 to 1.";//debug
-        private string elevatorOutOfRangeMsg = "elevator must be between -1 to 1.";//debug
-        private string throttleOutOfRangeMsg = "throttle must be between 0 to 1.";//debug*/
+        /* Error Messages */
+        private const string _aileronOutOfRangeMsg = "aileron must be between -1 to 1.";
+        private const string _rudderOutOfRangeMsg = "rudder must be between -1 to 1.";
+        private const string _elevatorOutOfRangeMsg = "elevator must be between -1 to 1.";
+        private const string _throttleOutOfRangeMsg = "throttle must be between 0 to 1.";
     }
 }
