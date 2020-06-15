d3b284caac8bfbaff959d193946df5ce2b371476
OmerMizrachi <omermizra@gmail.com>
2020-06-15 19:23:13 +0300

    saving progress

---
+++ FlightMobileServer/Controllers/ScreenshotController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightMobileServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ScreenshotController : ControllerBase
+    {
+        // Todo return jpg image from server.
+        [HttpGet]
+        public async Task<IActionResult> GetScreenshot() {
+            throw new NotImplementedException();
+        }
+    }
+}
