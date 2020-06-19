b8716010ccd607c147a1c493d3592d1cfeace4ab
SHON POZNER <shon4me@gmail.com>
2020-06-19 12:49:50 +0300

    test 2 my name

--- FlightMobileServer/ClientModels/FlightGearAsyncClient.cs
+++ FlightMobileServer/ClientModels/FlightGearAsyncClient.cs
@@ -12,6 +12,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using FlightMobileServer.Models;
+//shon test
 
 namespace FlightMobileServer.ClientModels {
     public class FlightGearAsyncClient : IAsyncTcpClient{
